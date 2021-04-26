cb6ecb48daee667901c9507f652b220451d1ca3b
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-26 17:47:22 +0530

    test

--- dataset/student-merge.R
+++ dataset/student-merge.R
@@ -1,5 +1,9 @@
-d1=read.table("student-mat.csv",sep=";",header=TRUE)
-d2=read.table("student-por.csv",sep=";",header=TRUE)
+setwd(paste0(getwd(), '/dataset'))
 
-d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
-print(nrow(d3)) # 382 students
+d1 <- read.table('student-mat.csv', sep = ';', header = TRUE)
+d2 <- read.table('student-por.csv', sep = ';', header = TRUE)
+
+d3 <- merge(d1, d2, by = c('school', 'sex', 'age', 'address', 'famsize', 'Pstatus', 'Medu', 'Fedu', 'Mjob', 'Fjob',
+                           'reason', 'nursery', 'internet'))
+
+print(nrow(d3))
